d68baa4289cec80dfa7fa816be60fd55872e32cc
Lucas <Lucas.fernandespro@icloud.com>
2026-05-14 18:45:08 +0200

    Initial Commit

---
+++ LingeFlowApp/LingeFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingeFlowAppApp.swift
+//  LingeFlowApp
+//
+//  Created by Lucas Fernandes on 14/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LingeFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
